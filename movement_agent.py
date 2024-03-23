033f3cd3b52a77b31b998215148052ab43b83a65
nhandzai <ducnhan1522004@gmail.com>
2024-03-23 20:49:41 +0700

    new rturn case

--- movement_agent.py
+++ movement_agent.py
@@ -27,25 +27,30 @@ class Movement:
     # dir: 1-↙ 2-↓ 3-↘ 4-← 6-→ 7-↖ 8-↑ 9-↗
     # pos(,): tuple contain position
     def move_agent(self, map, agent, pos, dir):
+        if dir == 5:
+            return pos
         self.update_movePos(pos)
         newPos = pos
+
         for move in self.movePos:
             if move[0] == dir:
                 newPos = (move[1], move[2])
+                break
+
         map[newPos[0]][newPos[1]] = agent
         map[pos[0]][pos[1]] = 0
         return newPos
 
     def find_path(self, hmap, pos):
-        minPath = hmap[pos[0]][pos[1]+1][1]
-        minPathPos = (pos[0],pos[1]+1)
-        
+        minPath = hmap[pos[0]][pos[1] + 1][1]
+        minPathPos = (pos[0], pos[1] + 1)
+
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if (i,j) != pos:
-                    if(minPath > hmap[pos[0]+i][pos[1]+j][1]):
-                        minPath = hmap[pos[0]+i][pos[1]+j][1]
-                        minPathPos = (pos[0]+i,pos[1]+j)
+                if (i, j) != pos:
+                    if minPath > hmap[pos[0] + i][pos[1] + j][1]:
+                        minPath = hmap[pos[0] + i][pos[1] + j][1]
+                        minPathPos = (pos[0] + i, pos[1] + j)
         self.update_movePos(pos)
         for move in self.movePos:
             if move[1] == minPathPos[0] and move[2] == minPathPos[1]:
