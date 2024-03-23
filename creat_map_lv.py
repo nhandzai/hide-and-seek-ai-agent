a1d9a54c54dc708d6c9540cad6cb65434de874e8
nhandzai <ducnhan1522004@gmail.com>
2024-03-23 20:48:43 +0700

    map size now is a tuple

--- creat_map_lv.py
+++ creat_map_lv.py
@@ -1,13 +1,15 @@
 def create_map_and_save(size, mapName):
-    mapData = [[0 for _ in range(size)] for _ in range(size)]
-    for i in range(size):
-        for j in range(size):
-            if i == 0 or i == size - 1 or j == 0 or j == size - 1:
+    mapData = [[0 for _ in range(size[1])] for _ in range(size[0])]
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if i == 0 or i == size[0] - 1 or j == 0 or j == size[1] - 1:
                 mapData[i][j] = 1
     with open(mapName, "w") as file:
-        file.write(str(size) + "\n")
+        file.write(f"{size[0]} {size[1]}\n")
         for row in mapData:
             file.write(" ".join(map(str, row)) + "\n")
     return mapData
 
-create_map_and_save(10,"maptroll.txt")
+
+size = (10, 10)
+create_map_and_save(size, "maptroll.txt")
--- h_map_generator.py
+++ h_map_generator.py
@@ -1,12 +1,12 @@
-def creat_h_map(row, col, mapData):
+def creat_h_map(pos, mapData):
     hmap = []
     for i in range(len(mapData)):
         rowData = []
         for j in range(len(mapData[i])):
-            if mapData[i][j] == 1 or mapData[i][j] == 5 :
-                rowData.append((1, 99))
+            if mapData[i][j] == 1 or mapData[i][j] == 5:
+                rowData.append((1, pos[0] * pos[1]))
             else:
-                dist = max(abs(row - i), abs(col - j))
+                dist = max(abs(pos[0] - i), abs(pos[1] - j))
                 rowData.append((mapData[i][j], dist))
         hmap.append(rowData)
 
