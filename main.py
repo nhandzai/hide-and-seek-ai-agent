16dff3c284eb994c8bd7607f95786060721ce0f7
PhamHoangKha1403 <118614332+PhamHoangKha1403@users.noreply.github.com>
2024-03-28 15:43:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def main():
     destination = (-1, -1)
     hmap = []
     turns = 1
-    seeker_turn = True
     pings = manager.pings
+    pos_dfs=(-1,-1);
+  
     
     screen, clock = MyGUI.create_screen_wrapper(map_data, manager)
     running = True
@@ -37,32 +38,33 @@ def main():
             if len(manager.hiders) == 0:
                 continue
             
-            if event.type == pygame.KEYDOWN:       
-                if seeker_turn:          
-                    destination = seeker.scan_target(map_data, 2)
-                    if destination == (-1, -1):
-                        seeker.explore()
+            if event.type == pygame.KEYDOWN:                 
+                destination = seeker.scan_target(map_data, 2)
+                if(turns==1):
+                    pos_dfs=seeker.find_pos_DFS(Manager.Manager.hmaps[seeker.pos] )
+                if destination == (-1, -1):
+                    if(turns<5):
+                        #print(pos_dfs)
+                        hmap=Manager.Manager.hmaps[pos_dfs] 
+                   # else:
+                    #    destination=seeker.nextDestination(pings)  
+                    seeker.explore()
 
-                    elif hider_last_seen_pos != destination:
-                        hider_last_seen_pos = destination
-                        hmap = Manager.Manager.hmaps[destination]
+                elif hider_last_seen_pos != destination:
+                    hider_last_seen_pos = destination
+                    hmap = Manager.Manager.hmaps[destination]
 
-                    # move the seeker
-                    seeker.move_wrapper(hmap, map_data)
-                    manager.check_hiders()
-                    
-                    seeker_turn = False
-                else:
-                    # ping
-                    if(turns % 5 == 0):
-                        manager.hiders_ping(map_data)
-                        print(pings)
-                    
-                    if config.HIDER_CAN_MOVE:
-                        manager.move_hiders(map_data)
-                    
-                    seeker_turn = True
-                    turns += 1
+                # move the seeker
+                seeker.move_wrapper(hmap, map_data)
+                manager.check_hiders()
+                
+                # ping
+                if(turns % 5 == 0):
+                    manager.hiders_ping(map_data)
+                    print(pings)
+                
+                if config.HIDER_CAN_MOVE:
+                    manager.move_hiders(map_data)
                     
                 # update the map
                 screen.draw_map(map_data, manager)
