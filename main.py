71675728320d886401168984abd89294dfcc76f4
nhandzai <ducnhan1522004@gmail.com>
2024-04-02 18:52:17 +0700

    fix last_seen not wroking bug

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import Seeker
 import Hider
 import ComputeHMap
 
+
 def main():
     map_data = ReadMap.read_map("map.txt")
     seeker = Seeker.Seeker(config.SEEKER_VIEW_RANGE, ReadMap.find_seeker(map_data))
@@ -14,66 +15,87 @@ def main():
     hiders = []
     id = 1
     for pos in hiders_pos:
-        hiders.append(Hider.Hider(config.HIDER_VIEW_RANGE, pos, config.HIDER_CAN_MOVE, id))
+        hiders.append(
+            Hider.Hider(config.HIDER_VIEW_RANGE, pos, config.HIDER_CAN_MOVE, id)
+        )
         id += 1
-        
+
     manager = Manager.Manager(seeker, hiders, map_data)
 
-    hider_last_seen_pos = (0, 0)    
+    hider_last_seen_pos = (-1, -1)
     destination = (-1, -1)
+    guessing_pos = (-1,-1)
     hmap = []
     turns = 1
     seeker_turn = True
+    chasing = False
     pings = manager.pings
-    pos_dfs = (-1,-1)
-    
+
     screen, clock = MyGUI.create_screen_wrapper(map_data, manager)
     running = True
-    
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             clock.tick(60)
-            
+
             if len(manager.hiders) == 0:
                 continue
-            
-            if event.type == pygame.KEYDOWN:       
-                if seeker_turn:          
+
+            if event.type == pygame.KEYDOWN:
+                if seeker_turn:
                     viewable_map = seeker.generate_viewable_map(map_data)
                     destination = seeker.scan_target(map_data, 2, viewable_map)
                     
-                    if turns == 1 and destination == (-1, -1):
-                        pos_dfs = seeker.find_pos_DFS(ComputeHMap.compute_h_map(map_data, seeker.pos))
-                    if destination == (-1, -1):
+                    if hider_last_seen_pos != (-1, -1) or destination != (-1, -1):
+                        chasing = True
+                        if destination != (-1,-1):
+                            hider_last_seen_pos = destination
+
+                    if chasing == True:
+                        hmap = ComputeHMap.compute_h_map(map_data, hider_last_seen_pos)
+                    else:
                         if turns < 5:
-                            hmap = ComputeHMap.compute_h_map(map_data, pos_dfs)
+                            guessing_pos = seeker.find_pos_DFS(
+                                ComputeHMap.compute_h_map(map_data, seeker.pos)
+                            )
+                        else:
+                            guessing_pos = seeker.find_pos_DFS(
+                                ComputeHMap.compute_h_map(map_data, seeker.pos)
+                            )
+                        hmap = ComputeHMap.compute_h_map(map_data, guessing_pos)                            
 
-                    elif hider_last_seen_pos != destination:
-                        hider_last_seen_pos = destination
-                        hmap = ComputeHMap.compute_h_map(map_data, destination)
-
-                    # move the seeker
                     seeker.move_wrapper(hmap, map_data)
-                    manager.check_hiders()
-                    
+                    if manager.check_hiders():
+                        chasing = False
+                        hider_last_seen_pos = (-1, -1)
+                        destination = (-1,-1)
+ 
+                    viewable_map = seeker.generate_viewable_map(map_data)
+                    destination = seeker.scan_target(map_data, 2, viewable_map)
+                    if hider_last_seen_pos != (-1, -1) or destination != (-1, -1):
+                        chasing = True
+                        if destination != (-1,-1):
+                            hider_last_seen_pos = destination
+
                     seeker_turn = False
                 else:
                     # ping
-                    if(turns % 5 == 0):
+                    if turns % 5 == 0:
                         manager.hiders_ping(map_data)
                         print(pings)
-                    
+
                     if config.HIDER_CAN_MOVE:
                         manager.move_hiders(map_data)
-                    
+                        
                     seeker_turn = True
                     turns += 1
-                    
+
                 # update the map
                 screen.draw_map(map_data, manager)
                 turns += 1
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
